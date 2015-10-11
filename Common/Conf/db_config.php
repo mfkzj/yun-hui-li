d284998a2e53901084480b2a2fc75402e93128ea
chenmingbiao <mbillchan@gmail.com>
2015-10-11 13:23:33 +0800

    修改数据库前缀

--- Common/Conf/db_config.php
+++ Common/Conf/db_config.php
@@ -3,10 +3,10 @@ return array (
   //本地
 	'DB_TYPE' => 'mysql',
 	'DB_HOST' => '127.0.0.1',
-	'DB_NAME' => 'yunfanli',
+	'DB_NAME' => 'yunhuili',
 	'DB_USER' => 'root',
 	'DB_PWD' => '',
 	'DB_PORT' => '3306',
-	'DB_PREFIX' => 'yfl_',
+	'DB_PREFIX' => 'yhl_',
 
 );
