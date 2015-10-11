26129b5f08e17facb7c14f0b7b19fb3c3b6304db
chenmingbiao <mbillchan@gmail.com>
2015-10-11 18:00:07 +0800

    merge

--- Common/Conf/db_config.php
+++ Common/Conf/db_config.php
@@ -1,11 +1,11 @@
 <?php
 return array (
-  //本地
+  	//本地
 	'DB_TYPE' => 'mysql',
 	'DB_HOST' => '127.0.0.1',
 	'DB_NAME' => 'yunhuili',
 	'DB_USER' => 'root',
-	'DB_PWD' => '',
+	'DB_PWD' => 'QWErty123456',
 	'DB_PORT' => '3306',
 	'DB_PREFIX' => 'yhl_',
 
