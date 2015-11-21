b6199c85ff3386b50ffff7a911fa5e8fd30741a7
chenmingbiao <mbillchan@gmail.com>
2015-11-22 00:33:14 +0800

    merge

--- Common/Conf/db_config.php
+++ Common/Conf/db_config.php
@@ -9,13 +9,4 @@ return array (
 	// 'DB_PORT' => '3306',
 	// 'DB_PREFIX' => 'yhl_',
 
-	//服务器
-	'DB_TYPE' => 'mysql',
-    'DB_HOST' => '127.0.0.1',
-    'DB_NAME' => 'yunhuili',
-    'DB_USER' => 'root',
-    'DB_PWD' => 'QWErty123456',
-    'DB_PORT' => '3306',
-    'DB_PREFIX' => 'yhl_',
-
 );
